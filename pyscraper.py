c56e9718f673d50567ddbde9e15f0c6a3d6f8c30
andefjx <junxiang.fan@pku.edu.cn>
2016-11-04 17:02:00 +0800

    First commit of 2 raw .py files

---
+++ pyscraper.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+'''
+Toy codes from the book Web Scraping with Python
+'''
+from urllib2 import urlopen, HTTPError
+from bs4 import BeautifulSoup
+import datetime, random, re
+
+'''
+Toy example from the book showing basic moves
+'''
+# def getTitle(url):
+#     try:
+#         html = urlopen(url)
+#     except HTTPError as e:
+#         return None
+#     try:
+#         bsObj = BeautifulSoup(html, "lxml")
+#         title = bsObj.body.h1
+#     except AttributeError as e:
+#         return None
+#     return title
+# 
+# title = getTitle("http://www.pythonscraping.com/exercises/exercise1.html")
+# if title == None:
+#     print("Title could not be found")
+# else:
+#     print(title)
+
+'''
+Toy example of HTML Parsing
+'''
+# random.seed(datetime.datetime.now()) # Set random number generator seed with the current system time, ensuring a nwe random path through Wikipedia articles every time the program is run
+# def getLinks(articleUrl):
+#     try:
+#         html = urlopen("http://en.wikipedia.org"+articleUrl)
+#     except HTTPError as e:
+#         return None
+#     try:
+#         bsObj = BeautifulSoup(html, "lxml")
+#     except AttributeError as e:
+#         return None
+#     return bsObj.find("div", {"id":"bodyContent"}).findAll("a", href=re.compile("^(/wiki/)((?!:).)*$"))
+# 
+# links = getLinks("/wiki/Kevin_Bacon")
+# 
+# while len(links) > 0:
+#     newArticle = links[random.randint(0, len(links)-1)].attrs["href"]
+#     print(newArticle)
+#     links = getLinks(newArticle)
+pages = set()
+def getLinks(pageUrl):
+    global pages
+    html = urlopen("http://en.wikipedia.org"+pageUrl)
+    bsObj = BeautifulSoup(html, "lxml")
+    for link in bsObj.find_all("a", href=re.compile("^(/wiki/)")):
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                # We have encountered a new page
+                newPage = link.attrs['href']
+                print(newPage)
+                pages.add(newPage)
+                getLinks(newPage)
+getLinks("")
---
+++ tinyscraper.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+import urllib, urllib2, re
+from cookielib import CookieJar
+from bs4 import BeautifulSoup
+'''
+Test code for obtaining web page
+'''
+# email = raw_input('Email: ')
+# passwd = raw_input('Passwd: ')
+# login_data = urllib.urlencode([
+#     ('username', email),
+#     ('password', passwd),
+#     ('entry', 'weibo'),
+#     ('client_id', ''),
+#     ('savestate', '1'),
+#     ('ec', ''),
+#     ('pagerefer', 'https://passport.weibo.cn/signin/welcome?entry=mweibo&r=http%3A%2F%2Fm.weibo.cn%2F')
+# ])
+# 
+# req = urllib2.Request('https://passport.weibo.cn/sso/login')
+# req.add_header('Origin', 'https://passport.weibo.cn')
+# req.add_header('User-Agent', 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
+# req.add_header('Referer', 'https://passport.weibo.cn/signin/login?entry=mweibo&res=wel&wm=3349&r=http%3A%2F%2Fm.weibo.cn%2F')
+# try:
+#     response = urllib2.urlopen(req, data=login_data.encode('utf-8'))
+# except urllib2.HTTPError as e:
+#     print 'The server couldn\'t fulfill the request.'
+#     print 'Error code: ', e.code
+# except urllib2.URLError as e:
+#     print 'We failed to reach a server.'
+#     print 'Reason: ', e.reason
+# else:
+#     the_page = response.read()
+#     print the_page
+
+'''
+Test code for Cookiejar
+'''
+# cookie = CookieJar()
+# cookie_support = urllib2.HTTPCookieProcessor(cookie)
+# opener = urllib2.build_opener(cookie_support)
+# opener.open('http://www.baidu.com')
+# for item in cookie:
+#     print(item.name+':'+item.value)
+
+'''
+Test code for getting cookie from xici
+'''
+headers=[('User-Agent','Mozilla/5.0 (Macintosh; Intel Mac OS 10_9_5) AppleWebKit 537.36 (KHTML, like Gecko) Chrome'),
+        ('Host', 'www.xicidaili.com'),
+        ('Referer', 'http://www.xicidaili.com/n')]
+def getCookie():
+    cookie = CookieJar()
+    cookie_support = urllib2.HTTPCookieProcessor(cookie)
+    opener = urllib2.build_opener(cookie_support)
+    opener.addheaders = headers
+    opener.open('http://www.xicidaili.com/')
+    return cookie
+
+'''
+Test code for BeautifulSoup
+'''
+html = urllib2.urlopen("http://www.pythonscraping.com/pages/page3.html")
+bs = BeautifulSoup(html,"lxml")
+for pic in bs.find_all('img', {'src':re.compile(".*\.jpg$")}):
+    print(pic['src'])
+
+'''
+Extract ips from Xici & save to local proxy.txt
+'''
+cookie = getCookie()
+# Get the proxy
+with open('proxy.txt', 'w') as f:
+    for page in range(1,1103):
+        if page%50 == 0: # Refresh cookie every 50 pages
+            print 'Refresh cookie and now starting crawling page%d' %(page)
+            cookie = getCookie()
+        url = 'http://www.xicidaili.com/nn/%s' %page
+        cookie_support = urllib2.HTTPCookieProcessor(cookie)
+        opener = urllib2.build_opener(cookie_support)
+        urllib2.install_opener(opener)
+        req = urllib2.Request(url, headers=dict(headers))
+        content = urllib2.urlopen(req)
+        soup = BeautifulSoup(content, "lxml")
+        trs = soup.find('table', id="ip_list").findAll('tr')
+        for tr in trs[1:]:
+            tds = tr.findAll('td')
+            ip = tds[1].text.strip()
+            port = tds[2].text.strip()
+            protocol = tds[5].text.strip()
+            f.write('%s://%s:%s\n' %(protocol, ip, port))
