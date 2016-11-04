9329a126abd912758aa5c63d35e6407b7e10d253
andefjx <junxiang.fan@pku.edu.cn>
2016-11-04 18:01:23 +0800

    Comment some lines

--- pyscraper.py
+++ pyscraper.py
@@ -49,17 +49,18 @@ Toy example of HTML Parsing
 #     newArticle = links[random.randint(0, len(links)-1)].attrs["href"]
 #     print(newArticle)
 #     links = getLinks(newArticle)
-pages = set()
-def getLinks(pageUrl):
-    global pages
-    html = urlopen("http://en.wikipedia.org"+pageUrl)
-    bsObj = BeautifulSoup(html, "lxml")
-    for link in bsObj.find_all("a", href=re.compile("^(/wiki/)")):
-        if 'href' in link.attrs:
-            if link.attrs['href'] not in pages:
-                # We have encountered a new page
-                newPage = link.attrs['href']
-                print(newPage)
-                pages.add(newPage)
-                getLinks(newPage)
-getLinks("")
+
+# pages = set()
+# def getLinks(pageUrl):
+#     global pages
+#     html = urlopen("http://en.wikipedia.org"+pageUrl)
+#     bsObj = BeautifulSoup(html, "lxml")
+#     for link in bsObj.find_all("a", href=re.compile("^(/wiki/)")):
+#         if 'href' in link.attrs:
+#             if link.attrs['href'] not in pages:
+#                 # We have encountered a new page
+#                 newPage = link.attrs['href']
+#                 print(newPage)
+#                 pages.add(newPage)
+#                 getLinks(newPage)
+# getLinks("")
